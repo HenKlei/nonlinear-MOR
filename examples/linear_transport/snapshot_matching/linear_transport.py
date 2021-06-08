fixed computation of error

examples/linear_transport/snapshot_matching/linear_transport.py
@@ -3,8 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import geodesic_shooting
-from geodesic_shooting.utils.io import save_image
-from geodesic_shooting.utils.visualization import plot_warpgrid, plot_vector_field
+from geodesic_shooting.utils.visualization import plot_vector_field
 
 
 if __name__ == "__main__":
@@ -15,15 +14,17 @@ if __name__ == "__main__":
         reference_solution = pickle.load(input_file)
 
     # perform the registration
-    geodesic_shooting = geodesic_shooting.GeodesicShooting(alpha=100., exponent=1.)
+    geodesic_shooting = geodesic_shooting.GeodesicShooting(alpha=1000., exponent=3.)
     image, v0, energies, Phi0, length = geodesic_shooting.register(reference_solution,
                                                                    full_solution, sigma=0.1,
-                                                                   epsilon=0.01, iterations=1500,
+                                                                   epsilon=0.01, iterations=5000,
                                                                    return_all=True)
 
-    norm = (np.linalg.norm((reference_solution - image).flatten())
-            / np.linalg.norm(reference_solution.flatten()))
+    norm = (np.linalg.norm((full_solution - image).flatten())
+            / np.linalg.norm(full_solution.flatten()))
     print(f'Relative norm of difference: {norm}')
+
+    plot_vector_field(v0)
 
     plt.matshow(reference_solution)
     plt.title("Input")

examples/linear_transport/snapshot_matching/parameters_input.py
@@ -7,7 +7,7 @@ import geodesic_shooting
 
 
 def main(alphas: Optional[List[float]] = Option([1., ], help=''),
-         exponents: Optional[List[float]] = Option([1., ], help=''),
+         exponents: Optional[List[int]] = Option([1., ], help=''),
          sigmas: Optional[List[float]] = Option([0.1, ], help=''),
          epsilons: Optional[List[float]] = Option([0.1, ], help='')):
 
@@ -27,8 +27,8 @@ def main(alphas: Optional[List[float]] = Option([1., ], help=''),
                                                     sigma=sigma, epsilon=epsilon, iterations=5000,
                                                     return_all=True)
 
-                    norm = (np.linalg.norm((reference_solution - image).flatten())
-                            / np.linalg.norm(reference_solution.flatten()))
+                    norm = (np.linalg.norm((full_solution - image).flatten())
+                            / np.linalg.norm(full_solution.flatten()))
                     with open('relative_errors.txt', 'a') as errors_file:
                         errors_file.write(f"{alpha}\t{exponent}\t{sigma}\t{epsilon}\t{norm}\n")
 

examples/linear_transport/snapshot_matching/try_different_parameters.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
                                                     sigma=sigma, epsilon=epsilon, iterations=5000,
                                                     return_all=True)
 
-                    norm = (np.linalg.norm((reference_solution - image).flatten())
-                            / np.linalg.norm(reference_solution.flatten()))
+                    norm = (np.linalg.norm((full_solution - image).flatten())
+                            / np.linalg.norm(full_solution.flatten()))
                     with open('relative_errors.txt', 'a') as errors_file:
                         errors_file.write(f"{alpha}\t{exponent}\t{sigma}\t{epsilon}\t{norm}\n")