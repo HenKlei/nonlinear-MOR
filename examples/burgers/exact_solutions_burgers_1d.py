f1b79b9b401937bc5e06c5abb6db2b09766f34cc
Hendrik Kleikamp <hendrik.kleikamp@uni-muenster.de>
2021-12-21 13:22:20 +0100

    improved and simplified Burgers example

--- examples/burgers/exact_solutions_burgers_1d.py
+++ examples/burgers/exact_solutions_burgers_1d.py
@@ -22,40 +22,48 @@ def exact_solution(x, *, mu=0.25):
                   * (0.5 + s_r * x[..., 1] - x[..., 0] > 0.)))
 
 
-fom = AnalyticalModel(exact_solution, n_x=N_X, n_t=N_T)
+def create_fom():
+    return AnalyticalModel(exact_solution, n_x=N_X, n_t=N_T)
 
 
-N_train = 5
-parameters = np.linspace(0.25, 1., N_train)
-reference_parameter = 0.25
+def main():
+    fom = create_fom()
 
-gs_smoothing_params = {'alpha': 100., 'exponent': 5}
-parameters_line_search = {'max_stepsize': 1., 'min_stepsize': 1e-4}
-registration_params = {'sigma': 0.1, 'parameters_line_search': parameters_line_search,
-                       'iterations': None}
-restarts = 10
+    N_train = 5
+    parameters = np.linspace(0.25, 1., N_train)
+    reference_parameter = 0.25
 
-reductor = NonlinearReductor(fom, parameters, reference_parameter,
-                             gs_smoothing_params=gs_smoothing_params)
-rom, output_dict = reductor.reduce(return_all=True, restarts=restarts,
-                                   registration_params=registration_params)
+    gs_smoothing_params = {'alpha': 100., 'exponent': 5}
+    parameters_line_search = {'max_stepsize': 1., 'min_stepsize': 1e-4}
+    registration_params = {'sigma': 0.1, 'parameters_line_search': parameters_line_search,
+                           'iterations': None}
+    restarts = 10
 
-with open('outputs/output_dict_rom', 'wb') as output_file:
-    pickle.dump(output_dict, output_file)
+    reductor = NonlinearReductor(fom, parameters, reference_parameter,
+                                 gs_smoothing_params=gs_smoothing_params)
+    rom, output_dict = reductor.reduce(return_all=True, restarts=restarts,
+                                       registration_params=registration_params)
 
-test_parameters = [0.5, 0.75]
-for test_parameter in test_parameters:
-    u_red = rom.solve(test_parameter)
-    plt.matshow(u_red)
-    plt.savefig(f'results/result_mu_{str(test_parameter).replace(".", "_")}.png')
-    plt.close()
-    u_full = fom.solve(test_parameter)
-    plt.matshow(u_full)
-    plt.savefig(f'results/full_solution_mu_{str(test_parameter).replace(".", "_")}.png')
-    plt.close()
-    plt.matshow(u_red - u_full)
-    plt.savefig(f'results/difference_mu_{str(test_parameter).replace(".", "_")}.png')
-    plt.close()
-    with open('results/relative_errors.txt', 'a') as errors_file:
-        errors_file.write(f"{test_parameter}\t"
-                          f"{np.linalg.norm(u_red - u_full) / np.linalg.norm(u_full)}\n")
+    with open('outputs/output_dict_rom', 'wb') as output_file:
+        pickle.dump(output_dict, output_file)
+
+    test_parameters = [0.5, 0.75]
+    for test_parameter in test_parameters:
+        u_red = rom.solve(test_parameter)
+        plt.matshow(u_red)
+        plt.savefig(f'results/result_mu_{str(test_parameter).replace(".", "_")}.png')
+        plt.close()
+        u_full = fom.solve(test_parameter)
+        plt.matshow(u_full)
+        plt.savefig(f'results/full_solution_mu_{str(test_parameter).replace(".", "_")}.png')
+        plt.close()
+        plt.matshow(u_red - u_full)
+        plt.savefig(f'results/difference_mu_{str(test_parameter).replace(".", "_")}.png')
+        plt.close()
+        with open('results/relative_errors.txt', 'a') as errors_file:
+            errors_file.write(f"{test_parameter}\t"
+                              f"{np.linalg.norm(u_red - u_full) / np.linalg.norm(u_full)}\n")
+
+
+if __name__ == "__main__":
+    main()
--- examples/burgers/read_output_dict.py
+++ examples/burgers/read_output_dict.py
@@ -1,4 +1,4 @@
-import pickle
+import numpy as np
 import matplotlib.pyplot as plt
 
 from geodesic_shooting.utils.visualization import plot_vector_field
@@ -6,41 +6,44 @@ from geodesic_shooting.utils.visualization import plot_vector_field
 from nonlinear_mor.utils.torch.early_stopping import SimpleEarlyStoppingScheduler
 from nonlinear_mor.utils.torch.neural_networks import FullyConnectedNetwork
 from nonlinear_mor.utils.torch.trainer import Trainer
+from nonlinear_mor.reductors import NonlinearNeuralNetworkReductor as NonlinearReductor
 
 
-with open('outputs/output_dict_rom', 'rb') as output_file:
-    output_dict = pickle.load(output_file)
+def main():
+    restarts = 10
 
-print(output_dict.keys())
+    from exact_solutions_burgers_1d import create_fom
+    fom = create_fom()
 
-print(output_dict['reduced_velocity_fields'].shape)
-v0 = output_dict['reduced_velocity_fields'].reshape((2, 100, 100))
-plot_vector_field(v0, title="Reduced initial vector field (C2S)", interval=2)
-plt.show()
+    N_train = 50
+    training_set = np.linspace(0.25, 1., N_train)
+    reference_parameter = 0.25
+
+    max_basis_size = 15
+
+    reductor = NonlinearReductor(fom, training_set, reference_parameter)
+    rom, output_dict = reductor.reduce(max_basis_size=max_basis_size, return_all=True, restarts=restarts,
+                                       full_velocity_fields_file='outputs/full_velocity_fields')
+
+    test_parameters = [0.5, 0.75]
+    for test_parameter in test_parameters:
+        u_red = rom.solve(test_parameter)
+        plt.matshow(u_red)
+        plt.savefig(f'results/result_mu_{str(test_parameter).replace(".", "_")}.png')
+        plt.close()
+        u_full = fom.solve(test_parameter)
+        plt.matshow(u_full)
+        plt.savefig(f'results/full_solution_mu_{str(test_parameter).replace(".", "_")}.png')
+        plt.close()
+        plt.matshow(u_red - u_full)
+        plt.savefig(f'results/difference_mu_{str(test_parameter).replace(".", "_")}.png')
+        plt.close()
+        relative_error = np.linalg.norm(u_red - u_full) / np.linalg.norm(u_full)
+        print(f"Parameter: {test_parameter}; Relative error: {relative_error}")
+        with open('results/relative_errors.txt', 'a') as errors_file:
+            errors_file.write(f"{test_parameter}\t"
+                              f"{relative_error}\n")
 
 
-validation_data = output_dict['validation_data']
-training_data = output_dict['training_data']
-layers_sizes = [1, 30, 30, 1]
-
-best_neural_network = None
-best_loss = None
-
-restarts = 100
-
-
-def train_neural_network(layers_sizes, training_data, validation_data):
-    neural_network = FullyConnectedNetwork(layers_sizes)
-    trainer = Trainer(neural_network, es_scheduler=SimpleEarlyStoppingScheduler)
-    best_loss, _ = trainer.train(training_data, validation_data)
-    return trainer.network, best_loss
-
-
-print(f"Performing {restarts} restarts of neural network training ...")
-for _ in range(restarts):
-    neural_network, loss = train_neural_network(layers_sizes, training_data, validation_data)
-    if best_loss is None or best_loss > loss:
-        best_neural_network = neural_network
-        best_loss = loss
-
-print(f"Trained neural network with loss of {best_loss} ...")
+if __name__ == '__main__':
+    main()
