57bb74e8c409fb35d5439f357bc1b83beaf4149a
Hendrik Kleikamp <hendrik.kleikamp@uni-muenster.de>
2022-04-04 14:10:19 +0200

    use typer in examples with exact solutions

--- examples/burgers/exact_solutions_burgers_1d.py
+++ examples/burgers/exact_solutions_burgers_1d.py
@@ -1,3 +1,4 @@
+from typer import Option, run
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,37 +10,36 @@ from nonlinear_mor.reductors import NonlinearNeuralNetworkReductor as NonlinearR
 from nonlinear_mor.models import AnalyticalModel
 
 
-N_X = 100
-N_T = 100
+def main(N_X: int = Option(100, help='Number of pixels in x-direction'),
+         N_T: int = Option(100, help='Number of pixels in time-direction'),
+         N_train: int = Option(50, help='Number of training parameters'),
+         reference_parameter: float = Option(0.25, help='Reference parameter'),
+         alpha: float = Option(100., help='Alpha'),
+         exponent: int = Option(2, help='Exponent'),
+         sigma: float = Option(0.1, help='Sigma'),
+         max_basis_size: int = Option(10, help='Maximum dimension of reduced basis'),
+         restarts: int = Option(10, help='Maximum number of training restarts')):
 
+    def exact_solution(x, *, mu=0.25):
+        s_l = 1.5 * mu
+        s_m = mu
+        s_r = 0.5 * mu
+        t_intersection = 0.25 / (s_l - s_r)
+        return ScalarFunction(data=(2. * (x[..., 1] <= t_intersection) * (0.25 + s_l * x[..., 1] - x[..., 0] >= 0.)
+                                    + (2. * (x[..., 1] > t_intersection)
+                                       * (0.25 + (s_l - s_m) * t_intersection + s_m * x[..., 1] - x[..., 0] >= 0.))
+                                    + (1. * (0.25 + s_l * x[..., 1] - x[..., 0] < 0.)
+                                       * (0.5 + s_r * x[..., 1] - x[..., 0] > 0.))))
 
-def exact_solution(x, *, mu=0.25):
-    s_l = 1.5 * mu
-    s_m = mu
-    s_r = 0.5 * mu
-    t_intersection = 0.25 / (s_l - s_r)
-    return ScalarFunction(data=(2. * (x[..., 1] <= t_intersection) * (0.25 + s_l * x[..., 1] - x[..., 0] >= 0.)
-                                + (2. * (x[..., 1] > t_intersection)
-                                   * (0.25 + (s_l - s_m) * t_intersection + s_m * x[..., 1] - x[..., 0] >= 0.))
-                                + (1. * (0.25 + s_l * x[..., 1] - x[..., 0] < 0.)
-                                   * (0.5 + s_r * x[..., 1] - x[..., 0] > 0.))))
+    def create_fom():
+        return AnalyticalModel(exact_solution, n_x=N_X, n_t=N_T, name='Analytical Burgers Model')
 
-
-def create_fom():
-    return AnalyticalModel(exact_solution, n_x=N_X, n_t=N_T, name='Analytical Burgers Model')
-
-
-def main():
     fom = create_fom()
 
-    N_train = 50
     parameters = np.linspace(0.25, 1.5, N_train)
-    reference_parameter = 0.25
 
-    gs_smoothing_params = {'alpha': 100., 'exponent': 2}
-    registration_params = {'sigma': 0.1}
-    max_basis_size = 2
-    restarts = 20
+    gs_smoothing_params = {'alpha': alpha, 'exponent': exponent}
+    registration_params = {'sigma': sigma}
 
     import time
     timestr = time.strftime("%Y%m%d-%H%M%S")
@@ -60,7 +60,7 @@ def main():
     pathlib.Path(results_filepath).mkdir(parents=True, exist_ok=True)
     test_parameters = [0.5, 0.75, 1., 1.25]
     for test_parameter in test_parameters:
-        u_red = rom.solve(test_parameter)
+        u_red = rom.solve(test_parameter, filepath_prefix=filepath_prefix)
         u_red.save(f'{results_filepath}/result_mu_{str(test_parameter).replace(".", "_")}.png')
         u_full = fom.solve(test_parameter)
         u_full.save(f'{results_filepath}/full_solution_mu_{str(test_parameter).replace(".", "_")}.png')
@@ -71,4 +71,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    run(main)
