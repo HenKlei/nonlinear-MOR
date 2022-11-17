e8de54639db2d3eb8720b89f31cf0e23069fdc48
Hendrik Kleikamp <hendrik.kleikamp@uni-muenster.de>
2022-11-17 16:15:05 +0100

    sample correct number of parameters from parameter space

--- nonlinear_mor/utils/parameters.py
+++ nonlinear_mor/utils/parameters.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from nonlinear_mor.utils.logger import getLogger
+
 
 class ParameterSpace:
     def __init__(self):
@@ -12,12 +14,16 @@ class CubicParameterSpace(ParameterSpace):
         self.extends = np.array(extends)
         self.dim = len(self.extends)
 
+        self.logger = getLogger('nonlinear_mor.CubicParameterSpace')
+
     def sample(self, num_samples=1, mode='uniform'):
         assert mode in ['uniform']
         assert isinstance(num_samples, int) and num_samples >= 1
 
         if mode == 'uniform':
-            linspace_list = [np.linspace(p_min, p_max, num_samples) for p_min, p_max in self.extends]
+            self.logger.info(f"Sampling {int(np.ceil(num_samples**(1/self.dim)))**self.dim} parameters uniformly ...")
+            linspace_list = [np.linspace(p_min, p_max, int(np.ceil(num_samples**(1/self.dim))))
+                             for p_min, p_max in self.extends]
             temp = np.meshgrid(*linspace_list)
             parameters = np.stack([temp_.T for temp_ in temp], axis=-1).reshape(-1, self.dim)
             assert parameters.shape == (num_samples**self.dim, self.dim)
