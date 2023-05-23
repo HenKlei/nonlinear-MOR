0130f2ebdbc3df425c02c39918db364aaa61f37c
Hendrik Kleikamp <hendrik.kleikamp@uni-muenster.de>
2023-05-23 15:26:24 +0200

    added examples of Burgers equation with one and two noninteracting shocks

---
+++ examples/1d/burgers/piecewise_constant/burgers_1d_analytical_one_shock.py
@@ -0,0 +1,15 @@
+from geodesic_shooting.core import ScalarFunction
+
+from nonlinear_mor.models import AnalyticalModel
+from nonlinear_mor.utils.parameters import CubicParameterSpace
+
+
+def create_model(spatial_shape, num_time_steps, spatial_extend=[(0., 1.)], temporal_extend=(0., 1.)):
+    def exact_solution(x, *, mu=1.):
+        assert mu > 0.
+        return ScalarFunction(data=(1. * (1./mu * (x[..., 0] - 0.25) - x[..., 1] <= 0.)))
+
+    parameter_space = CubicParameterSpace([(0.25, 1.5)])
+
+    return AnalyticalModel(spatial_shape, num_time_steps, parameter_space, exact_solution,
+                           spatial_extend, temporal_extend)
